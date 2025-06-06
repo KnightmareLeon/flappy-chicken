93d60d1f62561d4357f7445b3675666584d86af6
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-06-06 15:26:36 +0800

    perf(chicken): improve flapping

--- scripts/Chicken.cs
+++ scripts/Chicken.cs
@@ -1,23 +1,19 @@
 using Godot;
-using System;
 
 public partial class Chicken : CharacterBody2D
 {
 	[Export]
-	public int Speed { get; set; } = 50;
+	public int Speed { get; set; } = 100;
 	private Vector2 FallDirection { get; set; } = new Vector2(0,1);
-	public void GetInput()
-	{
-
-		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-		Velocity = inputDirection * Speed * 10;
-	}
+	private Vector2 FlapDirection { get; set; } = new Vector2(0,-1);
+	private int FlapTimer = 0;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		GetInput();
+		if (Input.IsActionJustPressed("up")){FlapTimer = 10;}
+
+		Velocity = (FlapTimer-- > 0) ? FlapDirection * Speed * 2 : FallDirection * Speed;
 		MoveAndSlide();
-		Velocity = FallDirection * Speed;
-		MoveAndSlide();
+
 	}
 }
