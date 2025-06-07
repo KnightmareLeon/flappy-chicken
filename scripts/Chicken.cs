e1af01073fd6c40830d81b0a94c24beb6a6e3106
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-06-07 22:15:08 +0800

    feat(chicken): add rotation animation

--- scripts/Chicken.cs
+++ scripts/Chicken.cs
@@ -7,12 +7,29 @@ public partial class Chicken : CharacterBody2D
 	private Vector2 FallDirection { get; set; } = new Vector2(0,1);
 	private Vector2 FlapDirection { get; set; } = new Vector2(0,-1);
 	private int FlapTimer = 0;
-
+	private float Tilt = 0f;
+	private const float TiltDegree = 0.0872665f;
 	public override void _PhysicsProcess(double delta)
 	{
-		if (Input.IsActionJustPressed("up")){FlapTimer = 10;}
-
-		Velocity = (FlapTimer-- > 0) ? FlapDirection * Speed * 2 : FallDirection * Speed;
+		if (Input.IsActionJustPressed("up")) { FlapTimer = 10; }
+		if (FlapTimer-- > 0)
+		{
+			Velocity = FlapDirection * Speed * 2;
+			if (Tilt < 0.785398f) // 45 degrees
+			{
+				Rotate(-TiltDegree * 2);
+				Tilt += TiltDegree * 2;
+			}
+		}
+		else
+		{
+			Velocity = FallDirection * Speed;
+			if (Tilt > -1.5708f) // 90 degrees
+			{
+				Rotate(TiltDegree);
+				Tilt -= TiltDegree;
+			}
+		}
 		MoveAndSlide();
 
 	}
