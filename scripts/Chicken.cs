a00cda6bca64792e57b35d0753df579acb920bab
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-06-09 16:10:02 +0800

    refactor: Lessen number of floor tiles and refactor Chicken.cs code

--- scripts/Chicken.cs
+++ scripts/Chicken.cs
@@ -4,11 +4,14 @@ public partial class Chicken : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set; } = 200;
-	private Vector2 FallDirection { get; set; } = new Vector2(0,1);
-	private Vector2 FlapDirection { get; set; } = new Vector2(0,-1);
 	private int FlapTimer = 0;
+
 	private float Tilt = 0f;
-	private const float TiltDegree = 0.0872665f;
+	private const float TILT_DEGREE = 0.0872665f;
+	
+	private Vector2 FallDirection { get; set; } = new Vector2(0, 1);
+	private Vector2 FlapDirection { get; set; } = new Vector2(0,-1);
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		if (Input.IsActionJustPressed("up")) { FlapTimer = 10; }
@@ -17,8 +20,8 @@ public partial class Chicken : CharacterBody2D
 			Velocity = FlapDirection * Speed * 3;
 			if (Tilt < 0.785398f) // 45 degrees
 			{
-				Rotate(-TiltDegree * 3);
-				Tilt += TiltDegree * 3;
+				Rotate(-TILT_DEGREE * 3);
+				Tilt += TILT_DEGREE * 3;
 			}
 		}
 		else
@@ -26,8 +29,8 @@ public partial class Chicken : CharacterBody2D
 			Velocity = FallDirection * Speed;
 			if (Tilt > -1.5708f) // 90 degrees
 			{
-				Rotate(TiltDegree);
-				Tilt -= TiltDegree;
+				Rotate(TILT_DEGREE);
+				Tilt -= TILT_DEGREE;
 			}
 		}
 		MoveAndSlide();
