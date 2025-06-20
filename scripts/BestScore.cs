31e823b12f92dbb50d1fd4d7bccafd52f4f5f10f
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-06-20 12:13:46 +0800

    feat: save best score

---
+++ scripts/BestScore.cs
@@ -0,0 +1,7 @@
+namespace Godot.Game.FlappyChicken;
+
+public partial class BestScore : Resource
+{
+    [Export]
+    public int SavedBestScore { get; set; }
+}
--- scripts/defeat_panel/DefeatPanel.cs
+++ scripts/defeat_panel/DefeatPanel.cs
@@ -4,6 +4,14 @@ public partial class DefeatPanel : PanelContainer
 {
     [Export]
     private Label _score;
+    [Export]
+    private Label _bestScoreLabel;
+    private BestScore _bestScoreResource;
+
+    public override void _Ready()
+    {
+        _bestScoreResource = (BestScore)ResourceLoader.Load("res://resources/best_score.tres");
+    }
 
     public override void _UnhandledInput(InputEvent @event)
     {
@@ -16,6 +24,13 @@ public partial class DefeatPanel : PanelContainer
     public void GetScore(int score)
     {
         _score.Text = score.ToString();
+        if (_score.Text.ToInt() > _bestScoreResource.SavedBestScore)
+        {
+            _bestScoreResource.SavedBestScore = _score.Text.ToInt();
+            ResourceSaver.Save(_bestScoreResource, "res://resources/best_score.tres");
+        }
+        _bestScoreLabel.Text = _bestScoreResource.SavedBestScore.ToString();
+
     }
 
 }
